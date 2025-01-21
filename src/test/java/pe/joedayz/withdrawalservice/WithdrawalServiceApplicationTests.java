1294c5a45c7780a9ab14caed0086f5b24de44473
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2025-01-20 22:08:57 -0500

    Withdraw service

---
+++ src/test/java/pe/joedayz/withdrawalservice/WithdrawalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.joedayz.withdrawalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithdrawalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
